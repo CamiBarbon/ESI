788299d6fa2f2f9297b64466a6059f651ed1fbae
Cami Barbon <camibarbon@gmail.com>
2025-07-29 16:54:45 -0300

    rutas estilos

--- js/descargar.js
+++ js/descargar.js
@@ -1,8 +1,8 @@
 function descargarArchivo(nombreArchivo) {
-    const link = document.createElement('a');
-    link.href = `./descargas/${nombreArchivo}`;  // ajustá la ruta si está en otra carpeta
-    link.download = nombreArchivo;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
+  const link = document.createElement('a');
+  link.href = `./descargas/${nombreArchivo}`;  // ajustá la ruta según tu estructura
+  link.download = nombreArchivo;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
