349e34ea7430016d2f4256841c1d6a03cefafb26
Cami Barbon <camibarbon@gmail.com>
2025-07-30 16:44:33 -0300

    Contenido ok

--- js/descargar.js
+++
@@ -1,8 +0,0 @@
-function descargarArchivo(nombreArchivo) {
-  const link = document.createElement('a');
-  link.href = `./descargas/${nombreArchivo}`;  // ajustá la ruta según tu estructura
-  link.download = nombreArchivo;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
